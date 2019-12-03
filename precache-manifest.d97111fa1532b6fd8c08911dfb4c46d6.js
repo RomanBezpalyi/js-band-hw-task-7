6e6527975306d5679b8e6f408815f215d90a6dba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-03 20:04:50 +0000

    Deploy RomanBezpalyi/js-band-hw-task-7 to github.com/RomanBezpalyi/js-band-hw-task-7.git:gh-pages

---
+++ precache-manifest.d97111fa1532b6fd8c08911dfb4c46d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5e411f0c6fe62e8a407d7396a8bb2d",
+    "url": "/js-band-hw-task-7/index.html"
+  },
+  {
+    "revision": "3d9581c332e5137be0e1",
+    "url": "/js-band-hw-task-7/static/css/2.1b28e48e.chunk.css"
+  },
+  {
+    "revision": "b3d5a5eb5681ca343004",
+    "url": "/js-band-hw-task-7/static/css/main.70f8cd4f.chunk.css"
+  },
+  {
+    "revision": "3d9581c332e5137be0e1",
+    "url": "/js-band-hw-task-7/static/js/2.8153e5be.chunk.js"
+  },
+  {
+    "revision": "b3d5a5eb5681ca343004",
+    "url": "/js-band-hw-task-7/static/js/main.daaf7ec9.chunk.js"
+  },
+  {
+    "revision": "82db8f84bbcc003ba0b0",
+    "url": "/js-band-hw-task-7/static/js/runtime-main.9cc15563.js"
+  },
+  {
+    "revision": "0aee39a996168344b3da0916f4c732db",
+    "url": "/js-band-hw-task-7/static/media/check-mark.0aee39a9.svg"
+  }
+]);
